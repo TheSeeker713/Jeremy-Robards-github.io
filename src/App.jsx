375b3c86ad1a81d87bf7f4893663c2dafd5b83f4
Jeremy Robards <jrobards713@gmail.com>
2025-09-25 13:10:43 -0600

    Deploy: Add built React app to root for GitHub Pages
    - Built React app successfully
    - Copied dist files to root for GitHub Pages deployment
    - Fixed missing Blog component import in App.jsx
    - Site should now load properly on www.jeremyrobards.com

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 // src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing.jsx";
-import Blog from "./pages/blog/Blog.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/blog" element={<Blog />} />
+      <Route path="/blog" element={<div>Blog coming soon...</div>} />
     </Routes>
   );
 }
